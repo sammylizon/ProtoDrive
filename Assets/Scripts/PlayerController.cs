5168e7d57d55ae3316649670347ff3166361d2ee
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2024-10-09 23:03:01 +0100

    Setting up camera to follow player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
